7863442e29f61148bf82089c76ba5a8e9f5cc40f
Maxi <maxijones012@gmail.com>
2018-05-16 17:15:06 -0300

    se movio el codigo a funciones separadas y modularizadas

--- main.c
+++ main.c
@@ -21,39 +21,81 @@
 #include "src/orden_det/orden_det.h"
 
 
+/*Declaraciones*/
+void ingresarEmpleado(int argc, char *argv[]);
+void ingresarCategoria(int argc, char *argv[]);
+//void mostrarArgumentos(int argc, char *argv[]){
 
 
 void cli_show(void *c)
 {
-  obj_cliente *cli = (obj_cliente*)c;
-     printf("ID:%d\n",cli->getClienteId(cli));
+  	obj_cliente *cli = (obj_cliente*)c;
+    	printf("ID:%d\n",cli->getClienteId(cli));
 }
 
+
+
+//--------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
-  obj_empleado *emp,*e_row;
-  obj_categoria *cat,*c_row;
-  obj_cliente *cli,*cli_row;
-  obj_proveedor *prov  ,*pv_row;
-  obj_producto *p,*p_rw;
-  obj_orden *o;
-  obj_orden_det *odet;
+	obj_empleado *emp,*e_row;
+	obj_categoria *cat,*c_row;
+	obj_cliente *cli,*cli_row;
+	obj_proveedor *prov  ,*pv_row;
+	obj_producto *p,*p_rw;
+	obj_orden *o;
+	obj_orden_det *odet;
+	  
+  	int result;
+  	int i=0,size=0;
+  	void *list;
   
-  int result;
-  int i=0,size=0;
-  void *list;
+  	//mostrarArgumentos(argc, argv);
   
-  /**int k;
-  	for (k=1; k<argc; k++){
-		printf("i: %d %s\n",k,argv[k]);
-	}
-  */	
   
-  //	printf("%s\n",argv[1]);
   	if  (!strcmp(argv[1],"-a")){
-  	//	printf("if 1\n");
 			if (!strcmp(argv[2],"-empleado")){
-			//printf("if 20\n\n\n\n");				
+			ingresarEmpleado(argc, argv);		
+		}
+
+		if(!strcmp(argv[2],"-categoria")){
+			ingresarCategoria(argc, argv);
+		}
+	}else{
+		if(!strcmp(argv[1],"-l")){
+			emp = empleado_new();
+  			listObj(emp,NULL,true,NULL);
+			
+			listObj(prov,NULL,true,NULL);
+			printf("\n----------------------------------\n");
+			cat = categoria_new();
+			listObj(cat,NULL,true,NULL);
+			printf("\n----------------------------------\n"); 
+			
+			p = producto_new();
+			listObj(p,NULL,true,NULL);
+			printf("\n----------------------------------\n");
+			
+		}
+	
+	}
+	
+  
+  
+  
+  	//esto estaba comentado
+	cat = categoria_new();
+	listObj(cat,NULL,true,NULL);  
+  	printf("\n----------------------------------\n");
+  	system("PAUSE");	
+  	return 0;
+}
+
+//-------------------------------------------------------------
+void ingresarEmpleado(int argc, char *argv[]){
+		
+			//printf("argv[4]: %s\n ", argv[4]);
+	
 			char *nombre; char *apellido; int reportar_a; int extension; char *fecha_nac;
 			int i=3;
   			for (i; i< argc; i++){
@@ -86,78 +128,54 @@ int main(int argc, char *argv[])
 				cat -> setExtension(cat, extension);
 				
 				
-				//cat -> saveObj(cat);			
-				if(!saveObj_empleadoImpl(cat)==1){
-					printf("salvo el objeto");	
-				}
+				cat -> saveObj(cat);			
+				/*if(!saveObj_empleadoImpl(cat)==1){
+				//	printf("salvo el objeto");	
+				}*/
 				
 				cat -> showObj(cat);
 				
 				//cat-> findbykey(cat));
 				
 				//habria que liberar el objeto
-			}  
-		}
-		
-		
-		
-		
-		
-		if(!strcmp(argv[2],"-categoria")){
-			//obj_categoria *cat1			
-			//cat1 = categoria_new();
-			
-			//setNombreCategoria();
-			printf("\n");
-			
-			
-		}
-	}
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  //esto estaba comentado
- ///*
-  
-  emp = empleado_new();
+			}
+};
 
-  listObj(emp,NULL,true,NULL);
-  printf("\n----------------------------------\n");
-  /*prov = proveedor_new();
-  listObj(prov,NULL,true,NULL);
-  printf("\n----------------------------------\n");
-  cat = categoria_new();
-  listObj(cat,NULL,true,NULL);
-  printf("\n----------------------------------\n"); 
-  
-  p = producto_new();
-  listObj(p,NULL,true,NULL);
-  printf("\n----------------------------------\n");
-  
-  */
-  system("PAUSE");	
-  return 0;
-}
+
+//-------------------------------------------------------------
+void ingresarCategoria(int argc, char *argv[]){
+		obj_categoria *cat1;			
+		cat1 = categoria_new();
+		
+		cat1 -> setNombreCategoria(cat1, argv[3]);
+		printf("\n");
+		
+		cat1 -> saveObj(cat1);
+		cat1 -> showObj(cat1);
+};
+
+//-------------------------------------------------------------
+
+/*
+void mostrarArgumentos(int argc, char *argv[]){
+	int k;
+  	for (k=1; k<argc; k++)
+		printf("i: %d %s\n",k,argv[k]);
+	
+
+};
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
